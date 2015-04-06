b9fcda420199263cdfaf9295136c2b8d09458f79
Moses Kim <moseskim92@gmail.com>
2015-04-06 16:16:48 -0700

    Update personsBirthday.c

--- personsBirthday.c
+++ personsBirthday.c
@@ -1,85 +1,99 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
 #include <linux/types.h>
-#include <linux/random.h>
-
+ 
 struct birthday {
-	int day;
-	int month;
-	int year;
-	char *name;
-	struct list_head list;
+    char name[10];
+    int day;
+    int month;
+    int year;
+    struct list_head list;
 };
-
-struct birthday *person, *tmp, *ptr;
-struct list_head *pos;
-
+ 
+static LIST_HEAD(birthday_list);
+ 
 /* This function is called when the module is loaded. */
-int person_init(void)
+int simple_init(void)
 {
-	int day, month, year;
-	int i = 0;
-	char *name;
-	person = kmalloc(sizeof(*person),GFP_KERNEL);
-	person->day = 2;
-	person->month = 8;
-	person->year = 1995;
-	person->name = "test";
+       
+ 
+    struct birthday *person, *ptr;
 
-	INIT_LIST_HEAD(&person->list);
-	printk(KERN_INFO "Loading Persons:\n");
+    printk(KERN_INFO "Loading Module\n");
 
-	while(i<5)
-	{
+    person = kmalloc (sizeof(*person), GFP_KERNEL);
+    strcpy(person->name,"person 1");
+    person->day=2;
+    person->month=8;
+    person->year=1995;
+    INIT_LIST_HEAD(&person->list);
+    list_add_tail(&person->list, &birthday_list);
+ 
+   person = kmalloc (sizeof(*person), GFP_KERNEL);
+    strcpy(person->name,"person 2");
+    person->day=6;
+    person->month=3;
+    person->year=1992;
+    list_add_tail(&person->list, &birthday_list);
 
-		ptr = kmalloc(sizeof(*ptr),GFP_KERNEL);
-		ptr->name = "person "+i;
-		get_random_bytes(&day, 1);
-	    ptr->day = day % 31;
-	    if(ptr->day < 0)
-	        ptr->day = ptr->day * -1;
+   person = kmalloc (sizeof(*person), GFP_KERNEL);
+    strcpy(person->name,"person 3");
+    person->day=1;
+    person->month=5;
+    person->year=1985;
+    list_add_tail(&person->list, &birthday_list);
 
-	    get_random_bytes(&month, 1);
-	    ptr->month = month % 12;
+   person = kmalloc (sizeof(*person), GFP_KERNEL);
+    strcpy(person->name,"person 4");
+    person->day=20;
+    person->month=10;
+    person->year=1994;
+    list_add_tail(&person->list, &birthday_list);
 
-	    if(ptr->month < 0)
-	        ptr->month = ptr->month * -1;
-
-	    get_random_bytes(&year, 1);
-	    ptr->year = (year % 99);
-
-	    if(ptr->year < 0)
-	        ptr->year = ptr->year * -1;
-
-	    list_add(&(ptr->list), &person->list);
-	    i = i +1;
+   person = kmalloc (sizeof(*person), GFP_KERNEL);
+    strcpy(person->name,"person 5");
+    person->day=16;
+    person->month=2;
+    person->year=2000;
+    list_add_tail(&person->list, &birthday_list);
+ 
+ 
+    list_for_each_entry(ptr, &birthday_list,list){
+    /*on each iteration ptr points*/
+    /*to the next birthday struct*/
+    printk(KERN_INFO "name:%s; day:%d, month:%d, year: %d\n", ptr->name,
+    ptr->day,
+    ptr->month,
+    ptr->year);
     }
-	
-	printk(KERN_INFO "Beginning to traverse the list:\n");
-    list_for_each(pos, &person->list){
-        tmp = list_entry(pos, struct birthday, list);
-        printk("%s birthday is %d/%d/%d\n",tmp->name, tmp->month, tmp->day, tmp->year);
-    }
-
-    return 0;
+ 
+       return 0;
 }
-
+ 
 /* This function is called when the module is removed. */
-void delete_persons(void) 
-{
-	list_for_each_entry(ptr, tmp, &person->list,list)
-	{
-		list_del(&ptr->list);
-		kfree(ptr);
-	}
-	printk(KERN_INFO "Deleting Persons!\n");
+void simple_exit(void) {
+ 
+    struct birthday *ptr, *next;
+    printk(KERN_INFO "Removing Module\n");
+ 
+    list_for_each_entry_safe(ptr,next,&birthday_list, list){
+    /*on each iteration ptr points*/
+    /*to the next birthday struct*/
+    printk(KERN_INFO "name:%s, day:%d, month:%d, year: %d\n",
+    ptr->name, ptr->day,
+    ptr->month,
+    ptr->year);
+    list_del(&ptr->list);
+    kfree(ptr);
+    }
 }
-
-/* Macros for registering module creating and deleting persons. */
-module_init( person_init );
-module_exit( delete_persons );
-
+ 
+/* Macros for registering module entry and exit points. */
+module_init( simple_init );
+module_exit( simple_exit );
+ 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
 MODULE_AUTHOR("SGG");
